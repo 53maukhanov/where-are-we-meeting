70de81e6e823e90da2d8bddb37fbc04adaa96305
Martin Ukhanov <martin.ukhanov@sonova.com>
2025-02-26 16:42:16 -0500

    Updated message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -328,7 +328,7 @@
 
 		{#if !currentRoom}
 			<p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-2xl">
-				Select Meeting Room
+				Select a Meeting Room
 			</p>
 		{/if}
 	</div>
